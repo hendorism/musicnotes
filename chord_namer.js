9fda8768e7808cb175cad70130a3538bed8b6b66
hendorism <hendorism@gmail.com>
2020-11-26 23:03:14 -0800

    ledger lines function
    Working more on function drawLedgerLines().
    X would be reset to starting value randomly when I hit keys in 6th octave. Ledger lines were all drawing in the wrong place. I have seen every kind of ledger line behavior you could think of.

---
+++ chord_namer.js
@@ -0,0 +1,8 @@
+// Aaron Dull 2020
+/*
+    function tellMeWhatChordThisIs(notesOfTheChord) {
+        //output chord name
+    }
+*/
+// I will design my own nomenclature for chords.
+// (based on existing ones)
