0c7d7a145deb06f4eb8c31b9b6fe10ab02bade70
Aaron Dull <hendorism@gmail.com>
2021-03-04 11:39:54 -0800

    house keeping and comments

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
-  $(document).ready(function(){
-    //jquery
+$(document).ready(function() { //jquery
 console.log("js/main.js has loaded");
-
-window.addEventListener("keydown", function(e) {
+window.addEventListener("keydown", function(e) { // prevent keys from scrolling the page
   // 37   39   38   40   32
   // <    >    ^    D    SP
-  // prevent space-bar and arrow-keys from scrolling the page
-  if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-      e.preventDefault();
+  if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) { // not sure
+      e.preventDefault(); // (prevent default behavior)
   }
-}, false);
-$(document).keypress(function(e){
-  notationConsoleDotLog(`keypress = ${e.which}`);
-  musicalInput(keyMap[e.which]);
+}, false); // false?
+$(document).keypress(function(e) {
+  notationConsoleDotLog( `keypress = ${e.which}` );
+  musicalInput( keyMap[e.which] );
 });
 
-$(document).keyup(function(e){
+$(document).keyup(function(e) {
   notationConsoleDotLog(`keyup = ${e.which}`);
   if (e.which == 8){
     startOver();
@@ -34,11 +31,11 @@ $(document).keyup(function(e){
   updateRender();
 })
 var keyMap = {
-  "122": "c3", "120": "d3",  "99": "e3", "118": "f3",  "98": "g3", "110": "a3", "109": "b3",
-  "97": "c4", "115": "d4", "100": "e4", "102": "f4", "103": "g4", "104": "a4", "106": "b4",
-  "113": "c5", "119": "d5", "101": "e5", "114": "f5", "116": "g5", "121": "a5", "117": "b5",
-  "49": "r1",  "50": "r2",  "51": "r3",  "52": "r4",  "53": "r5",  "54": "r6",  "55": "r7",
-  "219": "do", "221": "uo"
+  "122":"c3","120":"d3","99":"e3","118":"f3","98":"g3","110":"a3","109":"b3",
+  "97":"c4","115":"d4","100":"e4","102":"f4","103":"g4","104":"a4","106":"b4",
+  "113":"c5","119":"d5","101":"e5","114":"f5","116":"g5","121":"a5","117":"b5",
+  "49":"r1","50":"r2","51":"r3","52":"r4","53":"r5","54":"r6","55":"r7",
+  "219":"do","221":"uo"
 }
 let y = 50;
 let x = 15;
@@ -49,18 +46,18 @@ let tmar = 50;
 var scoreComponents = [];
 // var yValueOfNoteRelativeToMiddleLine;
 var yValuesOfNotesRelativeToMiddleLine = {
-  "b6": -70, "a6": -65, "g6": -60, "f6": -55, "e6": -50, "d6": -45, "c6": -40,
-  "b5": -35, "a5": -30, "g5": -25, "f5": -20, "e5": -15, "d5": -10, "c5":  -5,
-  "b4":   0, "a4":   5, "g4":  10, "f4":  15, "e4":  20, "d4":  25, "c4":  30,
-  "b3":  35, "a3":  40, "g3":  45, "f3":  50, "e3":  55, "d3":  60, "c3":  65
+  "b6":-70,"a6":-65,"g6":-60,"f6":-55,"e6":-50,"d6":-45,"c6":-40,
+  "b5":-35,"a5":-30,"g5":-25,"f5":-20,"e5":-15,"d5":-10,"c5": -5,
+  "b4":  0,"a4":  5,"g4": 10,"f4": 15,"e4": 20,"d4": 25,"c4": 30,
+  "b3": 35,"a3": 40,"g3": 45,"f3": 50,"e3": 55,"d3": 60,"c3": 65
 }
 //let displayAccidental;
 let accidentalValue = 0;
-function setAccidentalValue() {
-
-}
+//function setAccidentalValue() {
+//
+//}
 let octaveShift = 0;
-function shiftOctave(shiftdirection) {
+function shiftOctave( shiftdirection ) {
   if (shiftdirection == "u") {
     octaveShift += 1;
   } else if (shiftdirection == "d") {
@@ -69,7 +66,7 @@ function shiftOctave(shiftdirection) {
 }
 let noteNames = ["a", "b", "c", "d", "e", "f", "g"];
 
-/////////////////////////////drawing stuff to page
+///////////////////////===== drawing stuff to page =====/////////
 function musicalInput(musicalInputCode) {
   if (musicalInputCode != undefined) {
     if (noteNames.includes(musicalInputCode.slice(0,1))) {
