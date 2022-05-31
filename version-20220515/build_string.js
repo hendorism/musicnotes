aaabc7e26b5e7301e5ee70f443d3f7847eaa906b
hendorism <hendorism@gmail.com>
2022-05-30 21:38:08 -0700

    worked on 20220515

--- version-20220515/build_string.js
+++ version-20220515/build_string.js
@@ -1,7 +1,7 @@
 let builderString = "";
 window.addEventListener("keydown", function(e) {
-  let k = e.key;
-  consoleLog("key: " + k);
+  let k = '"' + e.key + '", ';
+  //consoleLog("key: " + k);
   if([32, 37, 38, 39, 40].indexOf(k) > -1) {
     e.preventDefault();
   }
