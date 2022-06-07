061b5c8d534c2ac67d11f41bb7acdae9893d598c
Aaron Dull <hendorism@gmail.com>
2022-06-06 20:01:10 -0700

    populated controls array version-20220603

--- version-20220515/build_string.js
+++ version-20220515/build_string.js
@@ -1,6 +1,6 @@
 let builderString = "";
 window.addEventListener("keydown", function(e) {
-  let k = '"' + e.key + '", ';
+  let k = e.key + ', ';
   //consoleLog("key: " + k);
   if([32, 37, 38, 39, 40].indexOf(k) > -1) {
     e.preventDefault();
@@ -8,3 +8,7 @@ window.addEventListener("keydown", function(e) {
   builderString += k;
   consoleLog(builderString);
 }, false);
+
+
+let arr = [81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219]; //qwerty - flatter - lowered
+let arr2 = [49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189]; //nums - sharper - raised
